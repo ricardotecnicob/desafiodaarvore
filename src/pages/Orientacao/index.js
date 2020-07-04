156393d8e4f55619775ccdb3714194034aa08638
3esolucoesti <67586610+3esolucoesti@users.noreply.github.com>
2020-07-03 21:11:43 -0300

    Parte 2 Concluida

--- src/pages/Orientacao/index.js
+++ src/pages/Orientacao/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-import { Container, BackgroundPage } from './styles';
+import Personagem from '../../assets/images/person.png';
+
+import { Container, BackgroundPage, Book2Apresentacao } from './styles';
 
 function Orientacao() {
   return (
     <Container>
         <BackgroundPage></BackgroundPage>
+        <Book2Apresentacao>
+            <div className="conteudo" >
+                <div className="areapersonagem" >
+                  <img src={Personagem} border="" alt="" />
+                </div>
+                <div className="areaDescricao" >
+                  Olá esse é o <span>Leitor</span> <br/>
+                  ele vai te acompanhar 
+                  nessa trajetoria!
+                </div>
+            </div>
+        </Book2Apresentacao>
     </Container>
   );
 }
--- src/pages/Orientacao/styles.js
+++ src/pages/Orientacao/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import backgroundImageGame from '../../assets/images/backgroundImageGame.jpg';
+import backgroundImageBook from '../../assets/images/book2.png';
 
 export const Container = styled.div`
     max-width: 1024px;
@@ -13,4 +14,49 @@ export const BackgroundPage = styled.div`
     background-position: center;
     background-size: cover;
     filter: blur(5px);
+`;
+
+export const Book2Apresentacao = styled.div`
+    position: absolute;
+    top: 200px;
+    z-index: 2;
+    background-image: url(${backgroundImageBook});
+    background-position: center;
+    background-size: cover;
+    width: 1024px;
+    height: 600px;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+
+    .conteudo{
+      max-width: 768px;
+      margin: auto;
+      display: flex;
+      align-items: center;
+    }
+
+    .areapersonagem{
+      height: 400px;
+      width: 50%;
+      margin-right: 30px;
+      
+    }
+
+    .areapersonagem img{
+      height: 100%;
+    }
+
+    .areaDescricao{
+      width: 50%;
+      font-family: 'Acme', sans-serif;
+      font-size: 60px;
+      text-align:center;
+
+      span{
+        color: #316C27;
+      }
+
+    }
+
 `;
